b554613ee689413dc25ad1a44d3828f4e6532223
SuperSheep18 <nothingwillgowrong@gmail.com>
2014-10-27 13:11:54 -0600

    Final push for fix.

--- Yodelit/src/com/example/yodelit/MainActivity.java
+++ Yodelit/src/com/example/yodelit/MainActivity.java
@@ -2,17 +2,9 @@ package com.example.yodelit;
 
 import android.app.Activity;
 import android.content.Intent;
-<<<<<<< HEAD
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-=======
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
->>>>>>> FETCH_HEAD
 
 public class MainActivity extends Activity {
 
@@ -20,18 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button uNButton = (Button) findViewById(R.id.uNButton); //Could be declared earlier with other variables for neatness.
-        Log.v("hi", "hi");
-        
-        uNButton.setOnClickListener(new View.OnClickListener() {
-    		@Override
-    		public void onClick(View v) {
-    			Intent forumActivity = new Intent(v.getContext(), ForumActivity.class);
-    			startActivity(forumActivity);
-    		}
-        });
-        
-        
     }
 
 
@@ -41,14 +21,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-<<<<<<< HEAD
-=======
-    
+  
     
 	public void login(View view) {
 		//Toast.makeText(this, "Archive", Toast.LENGTH_SHORT).show();
 		Intent intent = new Intent(MainActivity.this, HomeActivity.class);
 		startActivity(intent);
 	}
->>>>>>> FETCH_HEAD
 }
