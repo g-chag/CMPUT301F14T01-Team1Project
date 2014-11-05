47e3377751de4786b57bf3c86e27689d7ddbf07a
chag-g <chaggar1@ualberta.ca>
2014-11-05 14:55:19 -0700

    merged two YodelIt folders

--- Yodelit/gen/com/example/yodelit/R.java
+++ Yodelit/gen/com/example/yodelit/R.java
@@ -31,8 +31,7 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int apppic=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
         public static final int AddEchoButton=0x7f09000d;
