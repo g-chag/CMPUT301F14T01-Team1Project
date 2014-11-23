6df3f094d83866602e945dee63672e1831a801c4
g-chag <chaggar1@ualberta.ca>
2014-11-22 22:26:43 -0700

    Kinda got photo working-ish
    Opens gallery, but crashes once you select an image; working on
    cleaning up the code a bit now before proceeding

--- Yodelit/src/com/example/yodelit/AddPhotoActivity.java
+++ Yodelit/src/com/example/yodelit/AddPhotoActivity.java
@@ -1,5 +1,3 @@
-//code from: http://viralpatel.net/blogs/pick-image-from-galary-android-app/
-
 package com.example.yodelit;
 
 import android.app.Activity;
@@ -10,57 +8,70 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-
-public class AddPhotoActivity extends Activity {
-    
-	
-	private static int RESULT_LOAD_IMAGE = 1;
-	
-
+ 
+/**
+ * This activity displays the gallery image picker.
+ * It displays the image that was picked.
+ *
+ * @author ITCuties
+ *
+ */
+public class AddPhotoActivity extends Activity implements OnClickListener {
+ 
+    // Image loading result to pass to startActivityForResult method.
+    private static int LOAD_IMAGE_RESULTS = 1;
+     
+    // GUI components
+    private Button button;  // The button
+    private ImageView image;// ImageView
+     
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_yodel);
-        
-        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
-        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				
-				Intent i = new Intent(
-						Intent.ACTION_PICK,
-						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-				
-				startActivityForResult(i, RESULT_LOAD_IMAGE);
-			}
-		});
+         
+        // Find references to the GUI objects
+        button = (Button)findViewById(R.id.buttonLoadPicture);
+        image = (ImageView)findViewById(R.id.image);
+         
+        // Set button's onClick listener object.
+        button.setOnClickListener(this);
+         
     }
-    
-    
+ 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	super.onActivityResult(requestCode, resultCode, data);
-    	
-		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-			Uri selectedImage = data.getData();
-			String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-			Cursor cursor = getContentResolver().query(selectedImage,
-					filePathColumn, null, null, null);
-			cursor.moveToFirst();
-
-			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-			String picturePath = cursor.getString(columnIndex);
-			cursor.close();
-			
-			ImageView imageView = (ImageView) findViewById(R.id.imageView1);
-			imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-		
-		}
-    
-    
+        super.onActivityResult(requestCode, resultCode, data);
+         
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        if (requestCode == LOAD_IMAGE_RESULTS && resultCode == RESULT_OK && data != null) {
+            // Let's read picked image data - its URI
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+             
+            // Now we need to set the GUI ImageView data with data read from the picked file.
+            image.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+             
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close();
+        }
+    }
+     
+    @Override
+    public void onClick(View v) {
+        // Create the Intent for Image Gallery.
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+         
+        // Start new activity with the LOAD_IMAGE_RESULTS to handle back the results when image is picked from the Image Gallery.
+        startActivityForResult(i, LOAD_IMAGE_RESULTS);
     }
 }
--- Yodelit/src/com/example/yodelit/AddYodelActivity.java
+++ Yodelit/src/com/example/yodelit/AddYodelActivity.java
@@ -9,10 +9,18 @@
 package com.example.yodelit;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AddYodelActivity extends Activity {
@@ -65,4 +73,56 @@ public class AddYodelActivity extends Activity {
 	public void cancel(View view){
 		finish();
 	}
+	//code from: http://www.itcuties.com/android/pick-image-from-gallery/
+    // Image loading result to pass to startActivityForResult method.
+    private static int LOAD_IMAGE_RESULTS = 1;
+     
+    // GUI components
+    private Button button;  // The button
+    private ImageView image;// ImageView
+     
+    protected void onCreate1(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_yodel);
+         
+        // Find references to the GUI objects
+        button = (Button)findViewById(R.id.buttonLoadPicture);
+        image = (ImageView)findViewById(R.id.image);
+         
+        // Set button's onClick listener object.
+        button.setOnClickListener((OnClickListener) this);
+         
+    }
+ 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+         
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        if (requestCode == LOAD_IMAGE_RESULTS && resultCode == RESULT_OK && data != null) {
+            // Let's read picked image data - its URI
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+             
+            // Now we need to set the GUI ImageView data with data read from the picked file.
+            image.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+             
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close();
+        }
+    }
+     
+    public void onClick(View v) {
+        // Create the Intent for Image Gallery.
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+         
+        // Start new activity with the LOAD_IMAGE_RESULTS to handle back the results when image is picked from the Image Gallery.
+        startActivityForResult(i, LOAD_IMAGE_RESULTS);
+    }
 }
